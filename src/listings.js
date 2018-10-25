cde680612cd5fc9e4d63928cf7cf8ec066a22ccd
github username <github email address>
2018-10-25 17:38:37 -0400

    view rentals page construction

--- src/listings.js
+++ src/listings.js
@@ -12,3 +12,36 @@ function renderListing(obj) {
   rowDiv.appendChild(infoCard);
   listingsContainer.appendChild(rowDiv);
 }
+
+function renderRentalsToReturn()
+{
+  listings.forEach(listing =>
+  {
+    if(listing.user === loggedInUser)
+    {
+      let card = document.createElement('card');
+      card.classList += 'costumeCard';
+
+      let imageCard = document.createElement('img');
+      imageCard.src = obj['img_url'];
+      imageCard.className = "card-mg-top";
+
+      let cardHeader = document.createElement('div');
+      cardHeader.className = "card-header";
+      cardHeader.innerText = `${obj['name']}`;
+
+      let infoCard = document.createElement('p');
+      infoCard.className = 'card-text';
+      infoCard.innerText = `Gender: ${obj['gender']}\nSize: ${obj['size']}\nBody Part: ${obj['parts']}\nCategory: ${obj['category']}\nTheme: ${obj['theme']}\nSpookiness: ${obj['spookiness']}`;
+
+      card.appendChild(cardHeader);
+      card.appendChild(imageCard);
+      card.appendChild(infoCard);
+
+      rentalRetunsContainer.appendChild(card);
+    }
+
+  });
+
+}
+}
