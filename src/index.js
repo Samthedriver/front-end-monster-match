4a62a7fbe5e8aebcd99410ad8a23bb1fc2a42b3b
github username <github email address>
2018-10-25 10:54:02 -0400

    converting html for .js to index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 let loggedInUser = {};
-let loginDivInit = false;
 let welcomeContainer = {};
 let loginContainer = {};
 let newUserContainer = {};
 let optionsContainer = {};
+var costumes;
+var users;
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -16,13 +17,74 @@ function init()
   welcomeContainer = document.getElementById('welcomeContainer');
   loginContainer = document.getElementById('loginContainer');
   newUserContainer = document.getElementById('newUserContainer');
-  optionsContainer = document.getElementById('optionsContainer');
+  //optionsContainer = document.getElementById('optionsContainer');
   welcomeContainer.style.display = 'none';
   loginContainer.style.display = 'none';
   newUserContainer.style.display = 'none';
-  optionsContainer.style.display = 'none';
+  //optionsContainer.style.display = 'none';
 
-  loginOrNewUser();
+  addWelcomeContainerListeners();
+  addOptionsContainerListeners();
+  addLoginContainerListeners();
+  welcomeUser();
   Controller.getAllCostumes();
 
+}
+
+function addLoginContainerListeners()
+{
+  let loginBtn = document.getElementById('loginFormBtn');
+  loginFormBtn.addEventListener('click', addloginFormBtnListener);
+}
+
+function addloginFormBtnListener()
+{
+  event.preventDefault();
+  let username = document.getElementById('login_input').value;
+  loginUser(username);
+}
+
+function addOptionsContainerListeners()
+{
+  let createCostumeBtn = document.getElementById('createCostumeBtn');
+  createCostumeBtn.addEventListener('click', addCreateCostumeBtnListener);
+
+  let logoutBtn = document.getElementById('logoutBtn');
+  logoutBtn.addEventListener('click', addLogoutBtnListener);
+}
+
+function displayUserOptions()
+{
+  welcomeContainer.style.display = 'none';
+  loginContainer.style.display = 'none';
+  newUserContainer.style.display = 'none';
+  //optionsContainer.style.display = 'block';
+}
+
+function addWelcomeContainerListeners()
+{
+  document.getElementById('loginBtn').addEventListener('click', addLoginBtnListener);
+  document.getElementById('newUserBtn').addEventListener('click', addNewUserBtnListener);
+}
+
+function addLoginBtnListener()
+{
+  event.preventDefault();
+  displayLogin();
+}
+
+function displayLogin()
+{
+  welcomeContainer.style.display = 'none';
+  loginContainer.style.display = 'block';
+  newUserContainer.style.display = 'none';
+  //optionsContainer.style.display = 'none';
+}
+
+function welcomeUser()
+{
+  welcomeContainer.style.display = 'block';
+  loginContainer.style.display = 'none';
+  newUserContainer.style.display = 'none';
+  //optionsContainer.style.display = 'none';
 }
--- src/users.js
+++ src/users.js
@@ -1,67 +1,3 @@
-function loginOrNewUser()
-{
-  // let rowDiv = document.createElement('div');
-
-  // rowDiv.class="row";
-  // rowDiv.className="btn btn-secondary";
-
-  // let header = document.createElement('btn');
-  // header.class = "col-md-4";
-  // header.className = "btn btn-secondary";
-  // header.innerText = "Monster Match";
-
-
-  // let loginBtn = document.createElement('btn');
-  // loginBtn.id = "loginBtn";
-  // loginBtn.class = "col-md-4";
-  // loginBtn.innerHTML = `<button type="button" class="btn btn-secondary">Login</button>`;
-  document.getElementById('loginBtn').addEventListener('click', addLoginBtnListener);
-
-  // let newUserBtn = document.createElement('btn');
-  // newUserBtn.id = "newUserBtn";
-  // newUserBtn.class = "col-md-4";
-  // newUserBtn.innerHTML = `<button type="button" class="btn btn-secondary">New User</button>`;
-  document.getElementById('newUserBtn').addEventListener('click', addNewUserBtnListener);
-
-  // rowDiv.appendChild(header);
-  // rowDiv.appendChild(loginBtn);
-  // rowDiv.appendChild(newUserBtn);
-  // welcomeContainer.appendChild(rowDiv);
-  welcomeContainer.style.display = 'block';
-}
-
-function addLoginBtnListener()
-{
-  event.preventDefault();
-  welcomeContainer.style.display = 'none';
-
-  loginContainer.style.display = 'block';
-
-  let loginForm = document.createElement('form');
-  loginForm.id="login_form";
-  loginForm.class = "col-md-6";
-  loginForm.innerHTML = `<div class="form-group">
-    <input type="text" class="form-control" id="login_input" placeholder="Enter Username">
-  </div>
-  <button type="submit" id="loginFormBtn" class="btn btn-secondary">Login</button>`;
-  loginContainer.appendChild(loginForm);
-
-  let loginFormBtn = document.getElementById('loginFormBtn');
-  loginFormBtn.addEventListener('click', addloginFormBtnListener);
-  loginDivInit = true;
-
-}
-
-function addloginFormBtnListener()
-{
-  event.preventDefault();
-  welcomeContainer.style.display = 'none';
-  let username = document.getElementById('login_input').value;
-
-  loginUser(username);
-
-}
-
 function loginUser(username)
 {
   let found = false;
@@ -89,47 +25,12 @@ function loginUser(username)
       });
 }
 
-function displayUserOptions()
-{
-  loginContainer.style.display = 'none';
-  let container = document.getElementById('optionsContainer');
-  if(optionsCntainer.style.display === 'none')
-  {
-    optionsCntainer.style.display = 'block';
-    return;
-  }
-
-  let rowDiv = document.createElement('div');
-  rowDiv.innerHTML = `<div class="container">
-    <div class="row">
-      <div class="col-sm">
-        <button type="button" id="createCostumeBtn" class="btn btn-secondary">Create a costume</button>
-      </div>
-      <div class="col-sm">
-        <button type="button" id="viewCostumeBtn" class="btn btn-secondary">View your costumes</button>
-      </div>
-      <div class="col-sm">
-        <button type="button" id="logoutBtn" class="btn btn-secondary">Logout</button>
-      </div>
-    </div>
-  </div>`;
-
-  optionsCntainer.appendChild(rowDiv);
-  let createCostumeBtn = document.getElementById('createCostumeBtn');
-  createCostumeBtn.addEventListener('click', addCreateCostumeBtnListener);
-
-  let logoutBtn = document.getElementById('logoutBtn');
-  logoutBtn.addEventListener('click', addLogoutBtnListener);
-
-}
-
 function addLogoutBtnListener()
 {
   event.preventDefault();
   loggedInUser = {};
   console.log("in logout listener");
-  optionsContainer.style.display = "none";
-  welcomeContainer.style.display = "block";
+  welcomeUser();
 }
 
 function addCreateCostumeBtnListener()
@@ -142,29 +43,9 @@ function addNewUserBtnListener()
 {
   event.preventDefault();
 
-  //hiding welcome Div
   welcomeContainer.style.display = 'none';
 
-  if(newUserContainer.style.display === 'none')
-  {
-    newUserContainer.style.display = 'block';
-    return;
-  }
-
-  let loginForm = document.createElement('form');
-  loginForm.id="login_form";
-  loginForm.class = "col-md-6";
-  loginForm.innerHTML = `<div class="form-group">
-    <input type="text" class="form-control" id="newUserFirstName" placeholder="First Name">
-  </div>
-  <div class="form-group">
-    <input type="text" class="form-control" id="newUserLastName" placeholder="Last Name">
-  </div>
-  <div class="form-group">
-    <input type="text" class="form-control" id="newUserUserName" placeholder="Username">
-  </div>
-  <button type="submit" id="newUserFormBtn" class="btn btn-secondary">Create User</button>`;
-  newUserContainer.appendChild(loginForm);
+  newUserContainer.style.display = 'block';
 
   let newUserFormBtn = document.getElementById('newUserFormBtn');
   newUserFormBtn.addEventListener('click', addCreateNewUserBtnListener);
